a0ff092eac9149095dc7d2483b3451f53ebcc196
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WpfApplication57/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication57
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+    public class MyViewModel
+    {
+        public Item SomeObject
+        {
+            get;
+            set;
+        }
+        public ObservableCollection<Property> Properties
+        {
+            get;
+            set;
+        }
+        public MyViewModel()
+        {
+            SomeObject = new Item() { Property1 = "Value1", Property2 = "Value2", Property3 = "Value3", Property4 = "Value4", Property5 = "Value5", };
+            Properties = new ObservableCollection<Property>();
+            Properties.Add(new Property() { Name = "Property1" });
+            Properties.Add(new Property() { Name = "Property3" });
+            Properties.Add(new Property() { Name = "Property5" });
+        }
+    }
+    public class Property
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+    public class Item
+    {
+        public string Property1
+        {
+            get;
+            set;
+        }
+        public string Property2
+        {
+            get;
+            set;
+        }
+        public string Property3
+        {
+            get;
+            set;
+        }
+        public string Property4
+        {
+            get;
+            set;
+        }
+        public string Property5
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to: Generate Properties in PropertyGridControl From a Collection
+
+
+<p>This example demonstrates how to create <a href="https://documentation.devexpress.com/#WPF/CustomDocument15521">Property Definitions</a> and <a href="https://documentation.devexpress.com/#WPF/CustomDocument15719">Collection Definitions</a> based on a data source collection. For this, you can utilize the following properties.<br><br>The <a href="https://documentation.devexpress.com/#WPF/DevExpressXpfPropertyGridPropertyGridControl_PropertyDefinitionsSourcetopic">PropertyDefinitionsSource</a> property can be bound to a collection of custom objects.<br><br>The <a href="https://documentation.devexpress.com/WPF/DevExpressXpfPropertyGridPropertyGridControl_PropertyDefinitionTemplatetopic.aspx">PropertyDefinitionTemplate</a>, <a href="https://documentation.devexpress.com/WPF/DevExpressXpfPropertyGridPropertyGridControl_PropertyDefinitionStyletopic.aspx">PropertyDefinitionStyle</a>, <a href="https://documentation.devexpress.com/WPF/DevExpressXpfPropertyGridPropertyGridControl_PropertyDefinitionTemplateSelectortopic.aspx">PropertyDefinitionTemplateSelector</a> properties are used to configure generated Property Definition objects. If the PropertyDefinitionsSource collection contains PropertyDefinition objects, the use of these properties is unnecessary.</p>
+
+<br/>
+
+
