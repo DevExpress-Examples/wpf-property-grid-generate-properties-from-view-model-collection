f0382c300526de5a27a0154aa7f1e0b6cc4aaa72
DevExpressExampleBot <fakeEmail>
2023-02-23 19:09:04 +0400

    Source auto update [skip ci]

--- VB/WpfApplication57/MainWindow.xaml.vb
+++ VB/WpfApplication57/MainWindow.xaml.vb
@@ -1,12 +1,12 @@
+Imports DevExpress.Mvvm
+Imports System
+Imports System.Collections.Generic
 Imports System.Collections.ObjectModel
 Imports System.Windows
 Imports System.Windows.Controls
 
 Namespace WpfApplication57
 
-    ''' <summary>
-    ''' Interaction logic for MainWindow.xaml
-    ''' </summary>
     Public Partial Class MainWindow
         Inherits Window
 
@@ -15,36 +15,55 @@ Namespace WpfApplication57
         End Sub
     End Class
 
-    Public Class MyViewModel
+    Public Class PropertyDefinitionTemplateSelector
+        Inherits DataTemplateSelector
 
-        Public Property SomeObject As Item
+        Public Property PropertyTemplate As DataTemplate
+
+        Public Property CollectionTemplate As DataTemplate
+
+        Public Overrides Function SelectTemplate(ByVal item As Object, ByVal container As DependencyObject) As DataTemplate
+            Dim [property] As [Property] = CType(item, [Property])
+            If Not String.IsNullOrEmpty([property].CollectionName) Then Return CollectionTemplate
+            Return PropertyTemplate
+        End Function
+    End Class
+
+    Public Class ViewModel
+        Inherits ViewModelBase
+
+        Public Property EditObject As Employee
 
         Public Property Properties As ObservableCollection(Of [Property])
 
         Public Sub New()
-            SomeObject = New Item() With {.Property1 = "Value1", .Property2 = "Value2", .Property3 = "Value3", .Property4 = "Value4", .Property5 = "Value5"}
-            Properties = New ObservableCollection(Of [Property])()
-            Properties.Add(New [Property]() With {.Name = "Property1"})
-            Properties.Add(New [Property]() With {.Name = "Property3"})
-            Properties.Add(New [Property]() With {.Name = "Property5"})
+            EditObject = New Employee() With {.FirstName = "John", .LastName = "Smith", .Position = "Senior Developer", .BirthDate = New DateTime(1987, 12, 15), .PreviousPositions = New Positions() From {"Junior Developer", "Middle Developer"}}
+            Properties = New ObservableCollection(Of [Property]) From {New [Property]() With {.Name = "FirstName"}, New [Property]() With {.Name = "LastName"}, New [Property]() With {.Name = "Position"}, New [Property]() With {.Name = "BirthDate"}, New [Property]() With {.CollectionName = "PreviousPositions"}}
         End Sub
     End Class
 
     Public Class [Property]
 
         Public Property Name As String
+
+        Public Property CollectionName As String
     End Class
 
-    Public Class Item
+    Public Class Employee
 
-        Public Property Property1 As String
+        Public Property FirstName As String
 
-        Public Property Property2 As String
+        Public Property LastName As String
 
-        Public Property Property3 As String
+        Public Property BirthDate As Date
 
-        Public Property Property4 As String
+        Public Property Position As String
 
-        Public Property Property5 As String
+        Public Property PreviousPositions As Positions
+    End Class
+
+    Public Class Positions
+        Inherits List(Of String)
+
     End Class
 End Namespace
