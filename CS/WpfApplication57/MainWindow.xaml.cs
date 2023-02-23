ec679341b8643a9c637decd32a7d5881047e8e19
AndreySlabov <andrey.slabov@devexpress.com>
2023-02-23 19:05:45 +0400

    Update Sample

--- CS/WpfApplication57/MainWindow.xaml.cs
+++ CS/WpfApplication57/MainWindow.xaml.cs
@@ -1,86 +1,57 @@
-﻿using System;
+﻿using DevExpress.Mvvm;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace WpfApplication57
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+namespace WpfApplication57 {
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
         }
     }
-    public class MyViewModel
-    {
-        public Item SomeObject
-        {
-            get;
-            set;
-        }
-        public ObservableCollection<Property> Properties
-        {
-            get;
-            set;
-        }
-        public MyViewModel()
-        {
-            SomeObject = new Item() { Property1 = "Value1", Property2 = "Value2", Property3 = "Value3", Property4 = "Value4", Property5 = "Value5", };
-            Properties = new ObservableCollection<Property>();
-            Properties.Add(new Property() { Name = "Property1" });
-            Properties.Add(new Property() { Name = "Property3" });
-            Properties.Add(new Property() { Name = "Property5" });
+    public class PropertyDefinitionTemplateSelector : DataTemplateSelector {
+        public DataTemplate PropertyTemplate { get; set; }
+        public DataTemplate CollectionTemplate { get; set; }
+        public override DataTemplate SelectTemplate(object item, DependencyObject container) {
+            Property property = (Property)item;
+            if (!string.IsNullOrEmpty(property.CollectionName))
+                return CollectionTemplate;
+            return PropertyTemplate;
         }
     }
-    public class Property
-    {
-        public string Name
-        {
-            get;
-            set;
+    public class ViewModel : ViewModelBase{
+        public Employee EditObject { get; set; }
+        public ObservableCollection<Property> Properties { get; set; }
+        public ViewModel() {
+            EditObject = new Employee() {
+                FirstName = "John",
+                LastName = "Smith",
+                Position = "Senior Developer",
+                BirthDate = new DateTime(1987, 12, 15),
+                PreviousPositions = new Positions() { "Junior Developer", "Middle Developer" }
+            };
+            Properties = new ObservableCollection<Property> {
+                new Property() { Name = "FirstName" },
+                new Property() { Name = "LastName" },
+                new Property() { Name = "Position" },
+                new Property() { Name = "BirthDate" },
+                new Property() { CollectionName = "PreviousPositions" }
+            };
         }
     }
-    public class Item
-    {
-        public string Property1
-        {
-            get;
-            set;
-        }
-        public string Property2
-        {
-            get;
-            set;
-        }
-        public string Property3
-        {
-            get;
-            set;
-        }
-        public string Property4
-        {
-            get;
-            set;
-        }
-        public string Property5
-        {
-            get;
-            set;
-        }
+    public class Property {
+        public string Name { get; set; }
+        public string CollectionName { get; set; }
+    }
+    public class Employee {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Position { get; set; }
+        public Positions PreviousPositions { get; set; }
+    }
+    public class Positions : List<string> {
     }
 }
